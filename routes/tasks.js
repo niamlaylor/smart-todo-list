aa6d456a8fa94894597ec9eb8d085c67cbcbf065
Liam Naylor <liam@liamnaylor.com>
2023-01-05 19:09:03 -0800

    Update routes to add patch + refactor

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const router  = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('tasks'); // What we render depends on the frontend (what EJS templates do we have etc.)
-});
-
-router.post('/:task_id', (req, res) => {
-  res.render('tasks'); // This would create a new task. We will need a function in here that adds the task to the db
+router.post('/', (req, res) => {
+  console.log('Post request worked for tasks!');
   res.redirect('/');
 });
 
+router.get('/', (req, res) => {
+  res.render('tasks'); // What we render depends on the frontend (what EJS templates do we have etc.)
+  console.log('Get request worked for tasks!');
+});
+
+router.patch
+
+// For this route we delete the value of req.params
 router.delete('/:task_id', (req, res) => {
 });
 
