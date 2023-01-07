8f8a19c889970f1bb465d60f9f548ed160aff5ec
Liam Naylor <liam@liamnaylor.com>
2023-01-07 13:27:33 -0800

    Add api endpoint for all tasks access on frontend  /api/tasks

---
+++ routes/tasks-api.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const db = require('../db/connection');
+const taskQueries = require("../db/queries/tasks_queries");
+
+router.get('/', (req, res) => {
+  const query = `SELECT * FROM tasks`;
+  console.log(query);
+  db.query(query)
+    .then(data => {
+      const tasks = data.rows;
+      res.json({ tasks });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+});
+
+module.exports = router;
