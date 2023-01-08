66493ced416276e56015e54a9f44e1afeebd0e65
Lucas Sahota <lucaswade4@gmail.com>
2023-01-08 13:34:30 -0800

    Update movie-api to take parameters for task and userid

--- api/movie-api.js
+++ api/movie-api.js
@@ -1,14 +1,20 @@
 const axios = require("axios");
+const { addTask } = require("../db/queries/tasks_queries");
 
-const movieApi = function () {
+/**
+ *
+ * @param {*} task
+ * @param {*} userId
+ */
+const movieApi = function (task, userId) {
   axios
-    .get(`https://www.omdbapi.com/?t=${req.body["task-name"]}&apikey=a53781da`)
+    .get(`https://www.omdbapi.com/?t=${task}&apikey=a53781da`)
     .then((response) => {
       const parsedData = response.data;
       console.log(`Request successful! ${parsedData.Type}`);
       addTask({
-        user_id: req.session.user_id,
-        task_name: req.body["task_name"],
+        user_id: userId,
+        task_name: task,
         category: "To watch",
         due_date: new Date().toISOString(),
         date_created: new Date().toISOString(),
